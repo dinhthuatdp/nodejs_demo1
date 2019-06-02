6c075c1db699fdb0a1f3a5e2dc40ee593d2ea7e7
Dinh Thuat <dinhthuatdp@gmail.com>
2019-06-03 00:01:41 +0700

    Remove all code

--- Demo_Built-in_Modules/demo_built_in_assert.js
+++
@@ -1,5 +0,0 @@
-const builtinModules = require('builtin-modules');
-var assert = require('assert');
-//console.log(builtinModules);
-//assert(50 > 70, "My message goes here");
-assert(50 > 70);
--- demo_built_in_assert.js
+++
@@ -1,2 +0,0 @@
-var assert = require('assert');
-assert(5 > 7);
